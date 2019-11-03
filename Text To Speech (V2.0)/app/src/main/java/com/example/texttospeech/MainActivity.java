c991eb134fafef547d9edecd406842f6e8530c24
Katejay <jay.kate20@gmail.com>
2019-11-03 08:04:15 -0800

    V 2.0

--- Text To Speech (V2.0)/app/src/main/java/com/example/texttospeech/MainActivity.java
+++ Text To Speech (V2.0)/app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -7,14 +7,16 @@ import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    TextToSpeech t1, t2;
+    TextToSpeech tts1, tts2;
     EditText et1;
     Button b1,b2;
+    SeekBar sb1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +25,13 @@ public class MainActivity extends AppCompatActivity {
         et1=(EditText)findViewById(R.id.editText);
         b1=(Button)findViewById(R.id.button);
         b2=(Button)findViewById(R.id.button1);
+        sb1=(SeekBar)findViewById(R.id.seekBar1);
 
-
-        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        tts1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if(status != TextToSpeech.ERROR) {
-                    t1.setLanguage(Locale.UK);
+                    tts1.setLanguage(Locale.UK);
                 }
             }
         });
@@ -37,18 +39,21 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String toSpeak = et1.getText().toString();
-                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
-                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                String speak = et1.getText().toString();
+                float speed = (float) sb1.getProgress() / 50;
+                if (speed < 0.1) speed = 0.1f;
+                tts1.setSpeechRate(speed);
+                Toast.makeText(getApplicationContext(), speak,Toast.LENGTH_SHORT).show();
+                tts1.speak(speak, TextToSpeech.QUEUE_FLUSH, null);
             }
         });
 
 
-        t2=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        tts2=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if(status != TextToSpeech.ERROR) {
-                    t2.setLanguage(Locale.US);
+                    tts2.setLanguage(Locale.US);
                 }
             }
         });
@@ -56,23 +61,25 @@ public class MainActivity extends AppCompatActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String toSpeak = et1.getText().toString();
-                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
-                t2.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                String speak = et1.getText().toString();
+                float speed = (float) sb1.getProgress() / 50;
+                if (speed < 0.1) speed = 0.1f;
+                tts2.setSpeechRate(speed);
+                Toast.makeText(getApplicationContext(), speak,Toast.LENGTH_SHORT).show();
+                tts2.speak(speak, TextToSpeech.QUEUE_FLUSH, null);
             }
         });
-
     }
 
-    public void onPause(){
-        if(t1 !=null){
-            t1.stop();
-            t1.shutdown();
+    public void onDestroy(){
+        if(tts1 !=null){
+            tts1.stop();
+            tts1.shutdown();
         }
-        else if(t2 !=null){
-            t2.stop();
-            t2.shutdown();
+        else if(tts2 !=null){
+            tts2.stop();
+            tts2.shutdown();
         }
-        super.onPause();
+        super.onDestroy();
     }
 }
